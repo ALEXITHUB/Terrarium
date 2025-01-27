b1b70e74deda2f6f05a9f797e5c3fac8887f6c6a
alexis <alexquiatol@gmail.com>
2025-01-27 06:25:37 +0100

    Fonctionnalité double clique
    + double clliquer met l'image selectionée au 1er plan

--- page/Script.js
+++ page/Script.js
@@ -1,26 +1,31 @@
-dragElement(document.getElementById('plant1'));
-dragElement(document.getElementById('plant2'));
-dragElement(document.getElementById('plant3'));
-dragElement(document.getElementById('plant4'));
-dragElement(document.getElementById('plant5'));
-dragElement(document.getElementById('plant6'));
-dragElement(document.getElementById('plant7'));
-dragElement(document.getElementById('plant8'));
-dragElement(document.getElementById('plant9'));
-dragElement(document.getElementById('plant10'));
-dragElement(document.getElementById('plant11'));
-dragElement(document.getElementById('plant12'));
-dragElement(document.getElementById('plant13'));
-dragElement(document.getElementById('plant14'));
+//dragElement(document.getElementById('plant1'));
+//dragElement(document.getElementById('plant2'));
+//dragElement(document.getElementById('plant3'));
+//dragElement(document.getElementById('plant4'));
+//dragElement(document.getElementById('plant5'));
+//dragElement(document.getElementById('plant6'));
+//dragElement(document.getElementById('plant7'));
+//dragElement(document.getElementById('plant8'));
+//dragElement(document.getElementById('plant9'));
+//dragElement(document.getElementById('plant10'));
+//dragElement(document.getElementById('plant11'));
+//dragElement(document.getElementById('plant12'));
+//dragElement(document.getElementById('plant13'));
+//dragElement(document.getElementById('plant14'));
 
+document.querySelectorAll("[Id^='plant']").forEach(dragElement);
+
+//Function to drag elements aroound the terrarium
 function dragElement(terrariumElement)
 {
     //4 positions pour le positionnement dans l'écran
     let pos1 = 0,
         pos2 = 0,
         pos3 = 0,
-        pso4 = 0;
+        pos4 = 0;
+        highestZIndex = 1; //Keep the value of zIndex
     terrariumElement.onpointerdown = pointerDrag;
+    terrariumElement.addEventListener("dblclick", bringToFront);
 
     function pointerDrag(e)
     {
@@ -49,7 +54,14 @@ function dragElement(terrariumElement)
         document.onpointerup = null;
         document.onpointermove = null;
     }
+    //When double clicked, the element goes in front
+    function bringToFront()
+    {
+        highestZIndex++;
+        terrariumElement.style.zIndex = highestZIndex;
+    }
 }
+
 
 
  
